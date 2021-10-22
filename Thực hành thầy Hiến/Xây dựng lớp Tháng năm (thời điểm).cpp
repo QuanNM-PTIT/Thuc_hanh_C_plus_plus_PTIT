90cf5bf5bf344ea4e814795513aee46776a81d4a
Jimmy Nguyễn <84088181+cityuongbi@users.noreply.github.com>
2021-10-22 20:30:20 +0700

    Updated

--- Thực hành thầy Hiến/Xây dựng lớp Tháng năm (thời điểm).cpp
+++ Thực hành thầy Hiến/Xây dựng lớp Tháng năm (thời điểm).cpp
@@ -15,30 +15,37 @@ public:
         this -> thang  = 0;
         this -> nam = 0;
     }
+
     thangnam(int thang, int nam)
     {
         this -> thang = thang;
         this -> nam = nam;
     }
+
     void nhapbp()
     {
         cin >> this -> thang >> this -> nam;
     }
+
     void xemmh()
     {
         cout << "thang " << this -> thang << " nam " << this -> nam << endl;
     }
+
     operator char*()
     {
+		thangnam tmp;
+		tmp.thang = thang;
+		tmp.nam = nam;
         char *s = new char[105];
         stack<int> st;
-        while(thang)
+        while(tmp.thang)
         {
-            st.push(thang % 10);
-            thang /= 10;
+            st.push(tmp.thang % 10);
+            tmp.thang /= 10;
         }
-        char tmp[] = "thang ";
-        strcat(s, tmp);
+        char tmpc[] = "thang ";
+        strcat(s, tmpc);
         int i = 6;
         while(!st.empty())
         {
@@ -49,10 +56,10 @@ public:
         char tmp2[] = " nam ";
         i += 5;
         strcat(s, tmp2);
-        while(nam)
+        while(tmp.nam)
         {
-            st.push(nam % 10);
-            nam /= 10;
+            st.push(tmp.nam % 10);
+            tmp.nam /= 10;
         }
         while(!st.empty())
         {
@@ -62,6 +69,7 @@ public:
         }
         return s;
     }
+
     friend thangnam operator +(thangnam a, int n)
     {
         int tmp = a.thang;
@@ -76,6 +84,7 @@ public:
         }
         return a;
     }
+
     friend thangnam operator -(thangnam a, int n)
     {
         int tmp = a.thang;
@@ -90,6 +99,7 @@ public:
         }
         return a;
     }
+
     bool operator >(thangnam b)
     {
         if(nam > b.nam) return 1;
@@ -97,37 +107,35 @@ public:
         if(thang > b.thang) return 1;
         return 0;
     }
-};
 
-thangnam Thangnam(char* s)
-{
-    thangnam res;
-    int tmp = 0;
-    int i = 0;
-    while(!isdigit(s[i])) ++i;
-    while(isdigit(s[i]))
-    {
-        tmp = tmp * 10 + s[i] - '0';
-        ++i;
-    }
-    res.thang = tmp;
-    tmp = 0;
-    while(!isdigit(s[i])) ++i;
-    while(isdigit(s[i]))
-    {
-        tmp = tmp * 10 + s[i] - '0';
-        ++i;
-    }
-    res.nam = tmp;
-    return res;
-}
+	thangnam(char* s)
+	{
+		int tmp = 0;
+		int i = 0;
+		while(!isdigit(s[i])) ++i;
+		while(isdigit(s[i]))
+		{
+			tmp = tmp * 10 + s[i] - '0';
+			++i;
+		}
+		thang = tmp;
+		tmp = 0;
+		while(!isdigit(s[i])) ++i;
+		while(isdigit(s[i]))
+		{
+			tmp = tmp * 10 + s[i] - '0';
+			++i;
+		}
+		nam = tmp;
+	}
+};
 
 int main()
 {
     thangnam a, b(10, 2021), c;
     a.nhapbp();
     a.xemmh();
-    c = Thangnam((char*)"thang 11 nam 2021");
+    c = thangnam((char*)"thang 11 nam 2021");
     c.xemmh();
     cout << (char*)(c) << endl;
     c = a + 20;
